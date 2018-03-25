d7469763a44a9c3e2ca0b6bd63a7c34031aa897c
Matt Petty <matt@kizmeta.com>
2018-03-25 05:27:48 -0500

    Add TODOs

--- src/overshare.cr
+++ src/overshare.cr
@@ -8,4 +8,9 @@ when "server"
   Kemal.run
 when "settings"
   p Overshare::Settings
+when "+"
+  puts "TODO: Add file or url"
+  # TODO: ~/.config/overshare/settings.yml
+  # TODO: overshare + path/to/file.txt
+  # TODO: overshare + https://fonts.google.com
 end
