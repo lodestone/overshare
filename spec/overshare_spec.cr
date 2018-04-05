a0ec3110e3244a6553de78cb1e0127fc2b899aa4
Matt Petty <matt@kizmeta.com>
2018-04-05 14:34:17 -0500

    Use preferable, shorter url in specs

--- spec/overshare_spec.cr
+++ spec/overshare_spec.cr
@@ -13,7 +13,7 @@ describe Overshare do
   it "returns the requested file, unmodified" do
     setup_details
     Kemal.run do
-      get "/-/nested/twice/nested-twice.md"
+      get "/-nested/twice/nested-twice.md"
       response.body.should eq "# I am Nested Twice\n"
     end
   end
@@ -21,7 +21,7 @@ describe Overshare do
   it "parses the file if needed" do
     setup_details
     Kemal.run do
-      get "/-/nested/twice"
+      get "/-nested/twice"
       response.body.should match /.*\<h1.*\>I am Nested Twice\<\/h1\>.*/
     end
   end
@@ -29,7 +29,7 @@ describe Overshare do
   it "returns the requested file, unmodified when parsing is not needed" do
     setup_details
     Kemal.run do
-      get "/-/abc123"
+      get "/-abc123"
       response.status_code.should eq 302
       response.headers["Location"].should eq "/-/abc123/fu.pdf"
     end
@@ -38,7 +38,7 @@ describe Overshare do
   it "redirects when appropriate" do
     url = "http://nx.is"
     detail = Detail.make(url)
-    get "/-/#{detail.sid}"
+    get "/-#{detail.sid}"
     response.status_code.should eq 302
     response.headers["Location"].should eq url
   end
@@ -46,7 +46,7 @@ describe Overshare do
   it "returns the requested file, unmodified when parsing is not needed" do
     setup_details
     Kemal.run do
-      get "/-/nodata/i-have-no-data-file.html"
+      get "/-nodata/i-have-no-data-file.html"
       response.body.should eq "<html>\n<h1>No data.yml</h1>\n</html>\n"
     end
   end
