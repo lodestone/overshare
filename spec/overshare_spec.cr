63dd8cf74dfcdc52026e550a6ddb048674495908
Matt Petty <matt@kizmeta.com>
2018-04-04 19:07:32 -0500

    kemal spec start

--- spec/overshare_spec.cr
+++ spec/overshare_spec.cr
@@ -10,4 +10,20 @@ describe Overshare do
       response.body.should eq "# I am Nested Twice\n"
     end
   end
+
+  it "returns the requested file, unmodified" do
+    setup_details
+    Kemal.run do
+      get "/-/nested/twice/nested-twice.md"
+      response.body.should eq "# I am Nested Twice\n"
+    end
+  end
+
+  it "parses the file if needed" do
+    setup_details
+    Kemal.run do
+      get "/-/nested/twice"
+      response.body.should eq "# I am Nested Twice\n"
+    end
+  end
 end
